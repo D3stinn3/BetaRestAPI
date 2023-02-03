71383307972718eebfa46893412f1140c83ae1d2
D3stinn3 <paynedestinne@gmail.com>
2023-02-03 05:53:06 -0800

    Models design 2

--- drinks/settings.py
+++ drinks/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'drinks'
+    'drinks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
