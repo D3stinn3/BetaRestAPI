9c5f75559d89627a6291ab7bc6f0ea9155b774c4
D3stinn3 <paynedestinne@gmail.com>
2023-02-03 07:08:33 -0800

    Models design 4

--- drinks/models.py
+++ drinks/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+import os
 
 
 class Drink(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
 
+    def __str__(self):
+        jina = self.name
+        scribe = self.description
+
+        details_file = open('drinkdetails.txt', 'w')
+        details_file.write(str(jina))
+        details_file.close()
+
+        with open('drinkdetails.txt', 'r') as drinkDetails:
+            lines = drinkDetails.read()
+            print(lines)
+
+        return jina, scribe
