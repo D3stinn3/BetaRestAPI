b459e4cea5f7642249ece4a029446ca547e75273
D3stinn3 <paynedestinne@gmail.com>
2023-02-22 03:11:49 -0800

    Error Handling 1

--- drinks/views.py
+++ drinks/views.py
@@ -5,14 +5,35 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
+@api_view(['GET', 'POST'])
 def drink_list(request):
     # fetching the drinks
-    drinks = Drink.objects.all()
+    # drinks = Drink.objects.all()
 
     # analyzing by serializing
-    serializer = DrinkSerializer(drinks, many=True)
+    # serializer = DrinkSerializer(drinks, many=True)
 
     # return a json object
-    return JsonResponse(serializer.data, safe=False)
+    # return JsonResponse({'drinks': serializer.data}, safe=False)
+
+    try:
+
+        if request.method == 'GET':
+            drinks = Drink.objects.all()
+            serializer = DrinkSerializer(drinks, many=True)
+            return JsonResponse({'drinks': serializer.data}, safe=False)
+        if request.method == 'POST':
+            serializer = DrinkSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    except SystemError as e:
+        return 'Error: {0}'.format(e)
+
+"""Creating the api endpoints"""
